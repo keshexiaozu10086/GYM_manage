7def195da5feab607e031179fd35fe8d793403bb
mox <moxcoot@gmail.com>
2022-09-15 19:48:44 +0800

    更新注册和维修服务

--- src/main/java/cn/njust/Service/Customer/Repair.java
+++ src/main/java/cn/njust/Service/Customer/Repair.java
@@ -1,8 +1,14 @@
 package cn.njust.Service.Customer;
 
+import cn.njust.dao.EquipmentDao;
+import cn.njust.entity.Equipment;
+
 public class Repair {
-    public void repair(String eid)
+    public void repair(Equipment em)
     {
+        EquipmentDao dao =new EquipmentDao();
+        Equipment eq = dao.findEquipment(em);
+        eq.setState(2);//2代表停租
 
     }
 
